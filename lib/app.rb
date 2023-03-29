23c0e1507d02901df58e386bf9918e9f2fd1962f
tabatha wiggers <tatisabato@hotmail.com>
2023-03-29 15:44:35 -0300

    first commit

---
+++ lib/app.rb
@@ -0,0 +1,36 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+require_relative "../config/application"
+
+set :root, File.expand_path("..", __dir__)
+set :views, (proc { File.join(root, "app/views") })
+set :bind, '0.0.0.0'
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+get '/' do
+  # erb :index
+  # TODO
+  # 1. fetch posts from database.
+  # 2. Store these posts in an instance variable
+  # 3. Use it in the `app/views/posts.erb` view
+  @posts = Post.all
+  erb :posts # Do not remove this line
+end
+
+
+get "/about" do
+  erb :about
+end
+
+# TODO: add more routes to your app!
